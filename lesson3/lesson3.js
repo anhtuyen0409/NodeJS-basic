2b79dd6424f108a74e121d328e52e214114a6bae
Nguyen Anh Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-02-10 22:22:31 +0700

    su dung npm de quan li package va module trong nodejs

---
+++ lesson3/lesson3.js
@@ -0,0 +1,6 @@
+//sử dụng npm (node package manager) để quản lí các module
+//mở terminal -> cd lesson3 -> npm init -> enter đến entry point thì nhập index.js -> enter đến mục author nhập Tuyen Nguyen -> yes để confirm thông tin vừa khởi tạo npm
+//sau khi khởi tạo npm trong terminal -> hiển thị file package.json chứa các thông tin vừa khai báo trong project 
+//file package.json dùng để config các module ta sử dụng trong project
+//để cài đặt thêm module trong project:
+//ví dụ: Cài mysql -> vào terminal -> npm install mysql -g --save(-g có nghĩa cài ở chế độ global)
